ac697019d3d491b50152fe27f80908012bedfc3c
han28wy <42159035+han28wy@users.noreply.github.com>
2023-12-27 15:40:26 +0800

    OCT 提交

--- hot100/56.js
+++ hot100/56.js
@@ -1,5 +1,18 @@
 var merge = function(intervals) {
+    intervals = intervals.sort((a,b) => a[0]-b[0])
 
+    for(let i=1;i<intervals.length;i++){
+        // intervals[i][0]在上个区间中间
+        if(intervals[i][0] < intervals[i-1][1] || intervals[i][0] === intervals[i-1][1]){
+            let R= Math.max(intervals[i][1], intervals[i-1][1])
+            intervals[i-1] = [intervals[i-1][0], R]
+            intervals.splice(i,1)
+            i--
+        }
+    }
+    return intervals
 };
 
 intervals = [[1,3],[2,6],[8,10],[15,18]]
+const res = merge(intervals)
+console.log(res)
